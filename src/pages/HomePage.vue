b7b2b0bd0bc7c071cf55f2c54d2705d92b0cefa6
feinz2021 <84484716+feinz2021@users.noreply.github.com>
2022-06-16 01:32:18 +0800

    update auto create record and document if doesnt exist

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,42 @@
-<template>
-  home page 💣
-</template>
+<template>home page 💣</template>
 
 <script>
-export default {};
+import { setDoc, getDoc, doc } from "firebase/firestore";
+import firebase from "../utilities/firebase";
+
+export default {
+  mounted() {
+    const date = new Date();
+    const year = date.getFullYear();
+    let docName = "record" + year;
+
+    getDoc(doc(firebase.db, "record", docName)).then((docSnap) => {
+      if (docSnap.exists()) {
+        console.log("Record Exists.");
+      } else {
+        console.log("Record Doesn't Exists.");
+        setDoc(doc(firebase.db, "record", docName), {
+          year: year,
+          yearIncome: 0,
+          byMonthIncome: [
+            { january: 0 },
+            { february: 0 },
+            { march: 0 },
+            { april: 0 },
+            { may: 0 },
+            { june: 0 },
+            { july: 0 },
+            { august: 0 },
+            { september: 0 },
+            { october: 0 },
+            { november: 0 },
+            { december: 0 },
+          ],
+        });
+      }
+    });
+  },
+};
 </script>
 
 <style>
