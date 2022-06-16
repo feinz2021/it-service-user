154c9676665bea9cc540fb8b4d9d9e0768b6c12b
feinz2021 <84484716+feinz2021@users.noreply.github.com>
2022-06-16 15:00:31 +0800

    update test chartjs

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,10 +1,53 @@
-<template>home page 💣</template>
+<template>
+  home page 💣
+  <div class="container">
+    <Bar :chart-data="chartData" />
+  </div>
+</template>
 
 <script>
 import { setDoc, getDoc, doc } from "firebase/firestore";
 import firebase from "../utilities/firebase";
+import { Bar } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
 
 export default {
+  name: "BarChart",
+  components: { Bar },
+  data() {
+    return {
+      january: 3,
+      february: 4,
+      march: 5,
+      chartData: {
+        labels: ["January", "February", "March"],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: [40, 20, 12],
+          },
+        ],
+      },
+    };
+  },
   mounted() {
     document.title = "Homepage";
     window.M.AutoInit();
@@ -12,6 +55,7 @@ export default {
     const year = date.getFullYear();
     let docYearName = "record" + year;
 
+    // check if record exists
     getDoc(doc(firebase.db, "record", docYearName)).then((docSnap) => {
       if (docSnap.exists()) {
         console.log("Record Exists.");
@@ -24,7 +68,7 @@ export default {
         });
       }
     });
-
+    // check if documentID exists
     getDoc(doc(firebase.db, "record", "documentID")).then((docSnap) => {
       if (docSnap.exists()) {
         console.log("documentID counter Exists.");
@@ -35,6 +79,15 @@ export default {
         });
       }
     });
+
+    // assigning array for chartjs
+    //  console.log(this.chartData.datasets[0].data.length);
+    const testArray = [3,4,5];
+    const chartjsbar = this.chartData.datasets[0].data;
+    for (let i = 0; i < chartjsbar.length; i++) {
+      // console.log(chartjsbar[i]);
+      this.chartData.datasets[0].data[i] = testArray[i];
+    }
   },
 };
 </script>
