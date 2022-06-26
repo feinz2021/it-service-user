219b4cec8dbb318d663d3357145276e429ac0eee
feinz2021 <84484716+feinz2021@users.noreply.github.com>
2022-06-27 05:26:45 +0800

    update

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -6,13 +6,15 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyB39L-8WVL5ng6wUOO0G6N1VmKluQapAQo",
-    authDomain: "it-service-user.firebaseapp.com",
-    projectId: "it-service-user",
-    storageBucket: "it-service-user.appspot.com",
-    messagingSenderId: "652064831351",
-    appId: "1:652064831351:web:71f2cf7cfc681a30965372"
+    apiKey: process.env.VUE_APP_API_KEY,
+    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_APP_ID
 };
+
+console.log(process.env.VUE_APP_API_KEY)
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
